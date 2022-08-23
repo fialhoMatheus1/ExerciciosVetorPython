ee38889bd3b002e3f569803ac184016355ccb8c2
fialhoMatheus1 <104318023+fialhoMatheus1@users.noreply.github.com>
2022-08-22 21:18:18 -0300

    Add files via upload

--- ExerciciosVetorPython/Control.py
+++ ExerciciosVetorPython/Control.py
@@ -8,7 +8,11 @@ class Control:
     def menu(self):
         print("\n\nEscolha uma das opções abaixo:" +
               "\n0. Sair" +
-              "\n1. Exercício 1")
+              "\n1. Exercício 1" +
+              "\n3. Exercício 3" +
+              "\n5. Exercício 5" +
+              "\n6. Exercício 7" +
+              "\n9. Exercício 9")
         self.opcao = int(input())
 
     def operacao(self):
@@ -18,3 +22,26 @@ class Control:
                 print("Obrigado!")
             elif self.opcao == 1:
                 self.model.exercicio01()
+            elif self.opcao == 3:
+                self.model.exercicio03()
+            elif self.opcao == 5:
+                i = 0
+                for i in range(20):
+                    print("Informe o {}º número: ".format(i + 1))
+                    self.model.preencherVetor(int(input()))
+                self.model.ordenarDecrescente()
+            elif self.opcao == 7:
+                self.model.exercicio07()
+            elif self.opcao == 9:
+                i = 0
+                for i in range(10):
+                    print("Informe o {}º número: ".format(i + 1))
+                    self.model.preencherVetor(int(input()))
+                self.model.ordenarCrescente()
+                print("\nInsira um valor:")
+                valor = int(input())
+            elif self.opcao == 10:
+                self.model.teste()
+            else:
+                print("Opcão inválida!")
+
--- ExerciciosVetorPython/Model.py
+++ ExerciciosVetorPython/Model.py
@@ -2,8 +2,6 @@ class Model:
     def __init__(self):
         self.vetor = []
 
-    #metodo para receber os dados dos exercicios
-
     def exercicio01(self):
         nota = []
         media = 0
@@ -29,8 +27,8 @@ class Model:
 
     def exercicio03(self):
         q = []
-        menor =0
-        for i in range(5):
+        menor = 0
+        for i in range(3):
             print("Informe o {}° número:".format(i+1))
             num = int(input())
             while num < 0:
@@ -38,11 +36,57 @@ class Model:
                     print("Valor inválido.")
                     print("Informe o {}° número:".format(i+1))
                     num = int(input())
-                    if i == 0:
+            if i == 0:
                         menor = num
-                    else:
-                        if num < menor:
+            else:
+                if num < menor:
                             menor = num
             q.append(num)
-        #operações com vetor:
-        print("A menor valor de Q é: {}\nSua posição é: {}".format(menor))
+        print("A menor valor de Q é: {}\nSua posição é: (nao sei >:( ))".format(menor))
+
+    #exercicico 05:
+    def preencherVetor(self, num):
+            self.vetor.append(num)  # Incluindo dados no vetor
+
+    def visualizarVetor(self):
+            for i in range(len(self.vetor)):
+                print("{}º número: {}\n".format(i + 1, self.vetor[i]))
+    
+    def ordenarDecrescente(self):
+            self.vetor.sort(reverse=True)
+            self.visualizarVetor()
+
+    def exercicio07(self):
+        vetorGrau = []
+        soma = 0
+        menor = 0
+        maior = 0
+        media = 0
+        acimaMedia = 0
+        for i in range(365):
+            print("Informe uma temperatura:")
+            grau = int(input())
+            
+            if i == 0:
+                if grau < 0:
+                    menor = grau
+                if grau >= 0:
+                    maior = grau
+            else:
+                if grau < menor:
+                    menor = grau
+                if grau > maior:
+                    maior = grau
+
+            vetorGrau.append(grau)
+            soma = soma + grau
+        media = soma / 365
+        for i in range(365):
+            if vetorGrau[i] > media:
+                acimaMedia = acimaMedia + 1
+        print("Menor temperatura do ano: {}°\nMaior temperatura do ano: {}°\nMédia anual: {}°\nDias com temperatura acima da média: {} dias".format(menor, maior, media, acimaMedia))
+
+    #exercicio 9:
+    def ordenarCrescente(self):
+            self.vetor.sort()
+            self.visualizarVetor()
